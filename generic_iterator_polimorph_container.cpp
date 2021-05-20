8b8cc2c0e06688b35d30dca224fb4c3700222d58
Bjorn Hoffmann <hoffbjoe@gmail.com>
2021-05-20 09:09:59 +0200

    generalize iterator implementation and verify polimorph approach

--- generic_iterator_polimorph_container.cpp
+++ generic_iterator_polimorph_container.cpp
@@ -11,6 +11,16 @@ struct Check
 
 bool operator == (const Check& lhs, const Check& rhs) { return lhs.name == rhs.name; }
 
+template<class Value>
+class IteratorImpl
+{
+public:
+    using value_type = Value;
+    virtual value_type get() const = 0;
+    virtual void next() = 0;
+    virtual bool isEnd() const = 0;
+};
+
 template <class IteratorImpl, class Value>
 class Iterator
 {
@@ -34,15 +44,8 @@ public:
     virtual bool empty() const = 0;
     virtual size_t size() const = 0;
     
-    class CheckIteratorBase
-    {
-    public:
-        virtual Check get() const = 0;
-        virtual void next() = 0;
-        virtual bool isEnd() const = 0;
-    };
-    
-    using const_iterator = Iterator<CheckIteratorBase, Check>;
+    using IteratorBase = IteratorImpl<Check>;
+    using const_iterator = Iterator<IteratorImpl<Check>, Check>;
     virtual const_iterator begin() const = 0;
     virtual const_iterator end() const = 0;
 };
@@ -56,13 +59,15 @@ public:
     bool empty() const override { return checks_.empty(); }
     size_t size() const override { return checks_.size(); }
 
-    class CheckIterator final : public CheckIteratorBase
+    class CheckIterator final : public IteratorBase
     {
     public:
+        using IteratorBase::IteratorBase;
+        
         CheckIterator(const container& c) : cCurrent_{c.begin()}, cEnd_{c.end()} {}
         CheckIterator() = default;
 
-        Check get() const override { return cCurrent_ != cEnd_ ? *cCurrent_: Check{}; }
+        value_type get() const override { return cCurrent_ != cEnd_ ? *cCurrent_: Check{}; }
         void next() override { if (cCurrent_ != cEnd_) {++cCurrent_;} }
         bool isEnd() const override { return !(cCurrent_ != cEnd_); }
 
@@ -71,8 +76,41 @@ public:
         container::const_iterator cEnd_;
     };
     
-    const_iterator begin() const override { return const_iterator{std::unique_ptr<CheckIteratorBase>{new CheckIterator{checks_}}}; }
-    const_iterator end() const override { return const_iterator{std::unique_ptr<CheckIteratorBase>{new CheckIterator{}}}; }
+    const_iterator begin() const override { return const_iterator{std::unique_ptr<IteratorBase>{new CheckIterator{checks_}}}; }
+    const_iterator end() const override { return const_iterator{std::unique_ptr<IteratorBase>{new CheckIterator{}}}; }
+
+private:
+    container checks_{{"a"}, {"b"}, {"c"}};
+};
+
+class StoreCheckRepository final : public ICheckRepository
+{
+public:
+    using ICheckRepository::ICheckRepository;
+    using container = std::vector<Check>;
+
+    bool empty() const override { return checks_.empty(); }
+    size_t size() const override { return checks_.size(); }
+
+    class CheckIterator final : public IteratorBase
+    {
+    public:
+        using IteratorBase::IteratorBase;
+        
+        CheckIterator(const container& c) : cCurrent_{c.begin()}, cEnd_{c.end()} {}
+        CheckIterator() = default;
+
+        value_type get() const override { return cCurrent_ != cEnd_ ? *cCurrent_: Check{}; }
+        void next() override { if (cCurrent_ != cEnd_) {++cCurrent_;} }
+        bool isEnd() const override { return !(cCurrent_ != cEnd_); }
+
+    private:
+        container::const_iterator cCurrent_;
+        container::const_iterator cEnd_;
+    };
+    
+    const_iterator begin() const override { return const_iterator{std::unique_ptr<IteratorBase>{new CheckIterator{checks_}}}; }
+    const_iterator end() const override { return const_iterator{std::unique_ptr<IteratorBase>{new CheckIterator{}}}; }
 
 private:
     container checks_{{"a"}, {"b"}, {"c"}};
@@ -91,10 +129,10 @@ int main()
 {
     printf("Print check containers\n");
     print(MemoryCheckRepository{});
+    print(StoreCheckRepository{});
 
     return 0;
 }
-
 
 
 
